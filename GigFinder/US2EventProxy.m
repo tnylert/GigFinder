a9f5f05e097167afd239217965546155c4420a33
tnylert <Ty Lert>
2014-07-16 18:11:57 +0100

    Using NSURLSession to get data from Last.fm API

--- GigFinder/US2EventProxy.m
+++ GigFinder/US2EventProxy.m
@@ -8,11 +8,31 @@
 
 #import "US2EventProxy.h"
 
+
 @implementation US2EventProxy
 
 - (void)retrieveAllArtists
 {
     // NSURLSession
+    NSString *lastfmAPIKey = @"a2a238ddf3f5ac1370141bd0e3e3e646";
+    NSString *eventURL = [NSString stringWithFormat:@"http://ws.audioscrobbler.com/2.0/?method=event.getinfo&event=328799&api_key=%@&format=json", lastfmAPIKey];
+    __block NSString *jsonResponse;
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:eventURL]
+            completionHandler:^(NSData *data,
+                                NSURLResponse *response,
+                                NSError *error) {
+                // handle response
+                jsonResponse = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                NSLog(@"%@", jsonResponse);
+            }] resume];
+    
+    // Parse the string into JSON
+    //NSDictionary *json = [jsonResponse JSONValue];
+    
+    // Get the objects you want, e.g. output the second item's client id
+    //NSArray *items = [json valueForKeyPath:@"data.array"];
+    //NSLog(@" client Id : %@", [[items objectAtIndex:1] objectForKey:@"clientId"]);
     
     // If completed without error parse to US2Event
     
